10663959b4b621937a7c5a211901d847236abdbd
gundogduonur <gundogduonur@outlook.com.tr>
2023-10-27 22:35:24 +0300

    created model class

---
+++ src/main/java/com/onurgundogdu/model/User.java
@@ -0,0 +1,42 @@
+package com.onurgundogdu.model;
+
+import java.util.Date;
+
+public class User {
+
+    private String name;
+    private int age;
+    private Date birthDate;
+
+    public User(String name, int age, Date birthDate) {
+        this.name = name;
+        this.age = age;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Date getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(Date birthDate) {
+        this.birthDate = birthDate;
+    }
+}
+
+
